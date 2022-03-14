a20d71174f7871ddd75525c20fe0fd8d41bcfde9
Ronaldjga <Ronaldalmeida20022003@gmail.com>
2022-03-14 16:21:44 -0300

    config

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}', './src/patterns/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    './src/patterns/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        yelloPrimary: "#F2BF5E",
+        darkBluePrimary: "#022026",
+        grayPimary: "#022026",
+        bluePrimary: "#4B9EBF",
+        bluishGray: "#405F73",
+      },
+      fontFamily: {
+        sans: ['ui-sans-serif', 'system-ui',],
+        serif: ['ui-serif', 'Georgia',],
+        mono: ['ui-monospace', 'SFMono-Regular',],
+        h1Title: ['Cinzel',],
+        h2Title: ['"Domine"',],
+        body: ['"Work Sans"',],
+      }
+    },
+  },
+  plugins: [],
+}
