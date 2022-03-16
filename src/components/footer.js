7897388a4d0f038bda13aab3b40a1069c51298ad
Ronaldjga <Ronaldalmeida20022003@gmail.com>
2022-03-16 10:37:33 -0300

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,38 +1,26 @@
 import react from "react";
-import Image from 'next/image'
-
-import instagramIcon from './img/instagramIcon.svg'
-import twitterIcon from './img/twitterIcon.svg'
-import linkedinIcon from './img/linkedinIcon.svg'
 
 export function Footer() {
     return (
-        <footer
-            className="w-full h-[350px] bg-gray-900 p-5"
-        >
-            <h2 className="text-4xl font-h2Title font-bold text-center text-white">
-                Lista de Tarefas
-            </h2>
-            <SociaisIcon
-                src={instagramIcon}
-            />
+        <footer className="w-full h-[350px] bg-gray-900 flex justify-center items-center">
+            <div className="w-2/4 flex flex-col items-center justify-center gap-3">
+                <p className="text-yellowPrimary">Informações pessoais</p>
+                
+                <h2 className="text-center text-3xl font-h2Title text-white">Ronald Almeida</h2>
+                <button
+                    className="text-darkBluePrimary bg-yellowPrimary hover:brightness-125 py-2 px-5 font-bold"
+                >
+                    <a target={"_blank"} rel="noreferrer" href="https://meu-portifolio-one.vercel.app/">
+                        Meu Portifolio
+                    </a>
+                </button>
+                
+                <ul className="flex gap-2">
+                    <li><a target={"_blank"} rel="noreferrer" className="hover:text-yellowPrimary text-white" href="https://github.com/Ronaldjga">Github</a></li>
+                    <li><a target={"_blank"} rel="noreferrer" className="hover:text-yellowPrimary text-white" href="https://www.instagram.com/Ronald_jga/">Instagram</a></li>
+                    <li><a target={"_blank"} rel="noreferrer" className="hover:text-yellowPrimary text-white" href="https://www.linkedin.com/in/ronald-almeida-749198220/">LinkedIn</a></li>
+                </ul>
+            </div>
         </footer>
-    )
-}
-
-function SociaisIcon(props) {
-    return (
-        <button
-        className="w-[50px] h-[50px] relative"
-        >
-            <a href={`${props.href}`}>
-                <Image
-                    className="bg-yellowPrimary"
-                    layout="fill"
-                    src={props.src}
-                    alt={props.alt}
-                />
-            </a>
-        </button>
     )
 }
